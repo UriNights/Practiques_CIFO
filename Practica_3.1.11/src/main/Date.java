0fdd2483211e31e3b8976c1030747b0caa6a0c71
Oriol Calabuig <e.ocalabuig@gmail.com>
2020-10-21 11:29:35 +0200

    Actualitzat

--- Practica_3.1.11/src/main/Date.java
+++ Practica_3.1.11/src/main/Date.java
@@ -3,7 +3,8 @@ package main;
 public class Date {
 
 	public static final int[] daysForMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private static final String[] nameOfDay = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+	private static final String[] nameOfDay = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+			"Saturday" };
 
 	private int day, month, year;
 
@@ -18,45 +19,38 @@ public class Date {
 
 		int totalDays = 0;
 
-		if (Util.getMinimumDate().getYear() <= this.year) {
-
-			for (int i = Util.getMinimumDate().getYear(); i < this.year; i++) {
-				totalDays += (Util.isLeapYear(i)) ? 366 : 365;
-			}
+		// ---- Per ara no accepte dates que no superin la data mínima tant en el dia com el mes
+		
+		if (Util.getMinimumDate().getDay() < this.day) {
+			totalDays += this.day - Util.getMinimumDate().getDay();
 		}
-//----------------------****
-		if (Util.getMinimumDate().getMonth() <= this.month) {
+
+		if (Util.getMinimumDate().getMonth() < this.month) {
+
 			for (int i = Util.getMinimumDate().getMonth(); i < this.month; i++) {
-				
 				if (i == 2 && Util.isLeapYear(this.year)) {
 					totalDays += 29;
 					continue;
 				}
-				
-				totalDays += Date.daysForMonth[i];
-			}
-		} else if (this.month < Util.getMinimumDate().getMonth()) {
 
-			for (int i = this.month; i < Util.getMinimumDate().getMonth(); i++) {
-				
-				if (i == 2 && Util.isLeapYear(this.year)) {
-					totalDays -= 29;
-					continue;
-				}
-				
-				totalDays -= Date.daysForMonth[i];
+				totalDays += Date.daysForMonth[i - 1];
 			}
 		}
-		
-		totalDays += this.day - Util.getMinimumDate().getDay();
+
+		if (Util.getMinimumDate().getYear() < this.year) {
+
+			for (int i = Util.getMinimumDate().getYear(); i < this.year; i++) {
+
+				totalDays += (Util.isLeapYear(i)) ? 366 : 365;
+			}
+		}
 
 		return totalDays;
 	}
-	
-	public String dayName(int numberOfDays) {		// Counting from 1/1/1978 which is Sunday
-		return Date.nameOfDay[numberOfDays%7];
+
+	public String dayName(int numberOfDays) { // Counting from 1/1/1978 which is Sunday
+		return Date.nameOfDay[numberOfDays % 7];
 	}
-	
 
 	// Getters and Setters
 
